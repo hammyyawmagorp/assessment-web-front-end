a04052b161921aa5fa6bd24e8b96b24fd7a68917
hammyyawmagorp <zariktechnologies@gmail.com>
2022-05-16 13:13:25 -0400

    Updated HTML and JS

---
+++ about.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('Form has been succesfully submitted!');
+}
+
+
+let form = document.querySelector('form#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+
+function mouseoverComp(evt) {
+	alert('You are so good looking')
+}
+
+
+let catCompliment = document.querySelector('#cat-lion-logo')
+
+catCompliment.addEventListener('mouseover', mouseoverComp)
