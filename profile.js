b33b775711e608956740087da6952e9ddb493c39
Hamal Samuel <hamsam@Hamals-Mac-mini.fios-router.home>
2022-05-16 19:48:43 -0400

    Updated HTMl and JS. Added extra credit page

--- profile.js
+++ profile.js
@@ -9,10 +9,22 @@ function favPlaceButton(e){
 function favRitualButton(e){
     alert('My Favortie Ritual is going for a walk');
 }
+function feedMeBtn(e){
+    const restaurants = ['I Am Nacho Mama','Diwan Indian Food', 'La Finca Peruvian', 'Five Guys', 'Burger Village', 'La Taqueria', 'Uncle Dons Kitchen'];
+    const random = Math.floor(Math.random() * restaurants.length);
+
+    alert(`Try '${restaurants[random]}' today!`);
+
+}
+
 let favColorBtn = document.querySelector('#color');
 favColorBtn.addEventListener('click', favColorButton);
+
 let favPlaceBtn = document.querySelector('#place');
 favPlaceBtn.addEventListener('click', favPlaceButton);
+
 let favRitBtn = document.querySelector('#ritual');
 favRitBtn.addEventListener('click', favRitualButton);
 
+let foodBtn = document.querySelector('#food-recommend-btn');
+foodBtn.addEventListener('click', feedMeBtn);
